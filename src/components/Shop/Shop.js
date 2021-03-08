d01ac4a0aeaadcef6f9956c94fe3bda67f2c0fe9
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-08 22:22:19 +0600

    NaN solve in shop's order summery

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,16 +7,30 @@ import './Shop.css';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
+
     const [products, setProduct] = useState(first10);
     const [cart, setCart] = useState([]);
+
     const handleAddProduct = (product) =>{
-        const newCart = [...cart, product];
+        const toBeAddedKey = product.key;
+        const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
+        let count = 1;
+        let newCart;
+        if(sameProduct){
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = count ;
+            const others = cart.filter(pd => pd.key !== toBeAddedKey);
+            newCart = [...others, sameProduct];
+        }
+        else{
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        
         setCart(newCart);
-        // save local storage data
-        const sameProduct = newCart.filter(pd => pd.key === product.key);
-        const count = sameProduct.length;
         addToDatabaseCart(product.key, count);
     }
+
     return (
         <div className='shop-container'>
             <div className='product-container'>
