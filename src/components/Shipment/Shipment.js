b29f1d6572ac01aa2efd2e51ee882ad7f20fb2a5
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-18 12:56:54 +0600

    shipment form customized from react-hook-form

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import './Shipment.css';
+import { UserContext } from './../../App';
+
 
 const Shipment = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    const onSubmit = data => console.log(data);
+  
+    console.log(watch("example")); // watch input value by passing the name of it
+  
     return (
-        <div>
-            <h1>shipment page</h1>
+      
+    <div className="row">
+        <div className="col-3"></div>
+        <div className="col-6">
+
+            <form onSubmit={handleSubmit(onSubmit)} className="mt-3">
+                <input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} className='form-control' placeholder='Full name'/>
+                {errors.name && <span className="d-block text-danger">Name is required</span>}
+
+                <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} className='form-control mt-3' placeholder='Email'/>
+                {errors.email && <span className="d-block text-danger">Email is required</span>}
+
+                <input name="address" ref={register({ required: true })} className='form-control mt-3' placeholder='Shipping address'/>
+                {errors.address && <span className="d-block text-danger">Address is required</span>}
+
+                <input name="phone" ref={register({ required: true })} className='form-control mt-3' placeholder='Phone number'/>
+                {errors.phone && <span className="d-block text-danger">Phone number is required</span>}
+
+                <input className="btn btn-success mt-3" type="submit" />
+            </form>
+
         </div>
+        <div className="col-3"></div>
+    </div>
+      
     );
 };
 
