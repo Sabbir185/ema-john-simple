3ed11f869cad97ba4285f43995132b86359b57aa
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-03-29 16:47:53 +0600

    inventory modified

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import fakeData from './../../fakeData';
 
 const Inventory = () => {
     document.title = 'Inventory';
 
     const productHandler = () => {
+        const product = {};
         fetch('http://localhost:5000/addProducts',{
             method: 'POST',
             headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(fakeData)
+            body: JSON.stringify(product)
         })
     }
 
     return (
         <div>
-            <h1>Add Products</h1>
-            <button onClick={productHandler} className='btn btn-success' >Add Product</button>
+            
+            <form action="" className='text-center'>
+                <h1>Add Products</h1>
+                <p> <span>Name : </span> <input type="text" name="" id=""/> </p>
+                <p> <span>Price : </span> <input type="text" name="" id=""/> </p>
+                <p> <span>Quantity : </span> <input type="text" name="" id=""/> </p>
+                <p> <span>Image : </span> <input type="file" name="" id=""/> </p>
+                <button onClick={productHandler} className='btn btn-success' >Add Product</button>
+            </form>
         </div>
     );
 };
